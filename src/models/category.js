e8a569180288facd184b135f86a8d567b24df977
VIET HOANG <hoangnvph22264@fpt.edu.vn>
2023-10-06 01:03:10 +0700

    category

---
+++ src/models/category.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose')
+
+ const productSchema= new mongoose.Schema(
+    {   
+       
+        id_theloai:{type: mongoose.Schema.Types.ObjectId, ref:'categoryModel'}
+    },
+    {
+        collection:'product'
+    }
+ );
+ let  productModel= mongoose.model('productModel', productSchema);
+
+ const categoryiSchema= new mongoose.Schema(
+    {
+        name:{type:String, require:true}
+    },
+    {collection:'category'}
+ );
+ let categoryModel= mongoose.model('categoryModel',categoryiSchema);
+ module.exports={categoryModel, productModel};
