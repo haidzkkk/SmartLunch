3b5a74499b4f7f19f9b7b0637d4b990aded3a2ad
haidzkkk <117186588+haidzkkk@users.noreply.github.com>
2023-10-15 14:37:50 +0700

    Update comment.js

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,32 @@
+var mongoose = require('mongoose')
 
+
+const CommentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Auth",
+    required: true
+  },
+  productId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  collection: 'comments'
+});
+
+let commentModel = mongoose.model('Comment', CommentSchema)
+module.exports = commentModel
