1eb761536a88eaa39157db7744012785b5877fe4
huyanh0109 <huyanh0109@gmail.com>
2023-10-27 22:50:20 +0700

    statistics

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.render('main');
+  res.render('home');
 });
 module.exports = router;
