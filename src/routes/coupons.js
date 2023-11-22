906816387dc896685d6a96718862b7ebb379b8c1
huth2010 <117372286+huth2010@users.noreply.github.com>
2023-11-22 17:31:19 +0700

    sort data of notification

--- src/routes/coupons.js
+++ src/routes/coupons.js
@@ -8,7 +8,6 @@ router.get("/coupons", couponController.getAllCoupons);
 router.get("/coupons/:id", couponController.getOneCoupons);
 router.get("/delete/coupons/:id", couponController.removeCoupons);
 router.post("/coupons/:id", couponController.updateCoupons);
-
 router.get('/admin/coupons', couponController.getCouponUI);
 router.get('/admin/coupons/:id', couponController.getCouponIdUI);
 
