908bdbf63918df5cc5b0bb924a94acf3bc30f644
khuyennvph21768 <125357069+khuyennvph21768@users.noreply.github.com>
2023-12-16 00:32:43 +0700

    fix_ui_table

--- src/controllers/shopaddress.js
+++ src/controllers/shopaddress.js
@@ -1,14 +1,19 @@
-const Address = require('../models/shopaddress'); 
+const Address = require('../models/address');
+const NodeGeocoder = require('node-geocoder');
+const fetch = require('node-fetch');
+const options = {
+    provider: 'openstreetmap',
+};
 
-
+const geocoder = NodeGeocoder(options);
 exports.getAddressUI = async (req, res) => {
     const response = await fetch('http://localhost:3000/api/shopaddress');
     const data = await response.json();
-    if(data.length == 0){
-        res.render('shopaddress/address', { layout :"Layouts/home"});
+    if (data.length == 0) {
+        res.render('shopaddress/address', { layout: "Layouts/home" });
     }
-    if(data.length != 0){
-        res.render('shopaddress/updateaddress', { layout :"Layouts/home"});
+    if (data.length != 0) {
+        res.render('shopaddress/updateaddress', { layout: "Layouts/home" });
     }
 };
 
@@ -30,11 +35,48 @@ exports.getAddress = async (req, res) => {
 
         return res.status(200).json(
             address
-           
+
         )
     } catch (error) {
         return res.status(400).json({
             message: error.message
         })
     }
+}
+
+exports.add = async (req, res) => {
+    try {
+        const {
+          shop_name,
+          shop_number,
+          city,
+          district,
+          ward,
+          shop_detail
+        } = req.body;
+
+        // const addressString = 'Phường An Hải Bắc, Quận Sơn Trà, Thành phố Đà Nẵng';
+//   const geocodeResult = await geocoder.geocode(addressString);
+        const geocodeResult = await geocoder.geocode(`${ward}, ${district}, ${city}`);
+        if (!geocodeResult || geocodeResult.length === 0) {
+            return res.status(400).json({ error: 'Invalid address. Geocoding failed.' });
+          }
+        const { latitude, longitude } = geocodeResult[0];
+
+        const newAddress = new Address({
+          recipientName: shop_name,
+          phoneNumber: shop_number,
+          userId :"6576d6dd93ab304284ef12ac",
+          addressLine: `${ward}, ${district}, ${city}`,
+          latitude,
+          longitude,
+       
+        });
+        await newAddress.save();
+    
+        res.status(200).json({ message: 'Address created successfully' ,latitude,longitude});
+      } catch (error) {
+        console.error('Error creating address:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
 }
--- src/routes/shopaddress.js
+++ src/routes/shopaddress.js
@@ -4,6 +4,9 @@ const addressController = require('../controllers/shopaddress');
 const router = express.Router();
 router.get('/admin/shopaddress', addressController.getAddressUI);
 router.get('/shopaddress', addressController.getAddress);
-router.post('/shopaddress', addressController.updateAddress);
+// router.post('/shopaddress', addressController.updateAddress);
+router.post('/shopaddress', addressController.add);
+
+
 
 module.exports = router;
